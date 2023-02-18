e4a82d05f8d6c45970aca38266536143b55e926a
Bjorn Zschernack <dergoldbroiler@gmail.com>
2023-02-18 11:15:29 +0100

    setup

---
+++ components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Container from 'react-bootstrap/Container';
+
+export const Header = () => {
+    return (
+    <header className="header p-0">
+        <Container fluid>aa</Container>
+    </header>
+    )
+}
